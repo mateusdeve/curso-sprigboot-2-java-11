ee1c68fc21df8d3d8ea9dd8b3192d169e8f417f6
Mateus Pires <mateus.dev.ti@gmail.com>
2020-07-19 21:52:45 -0300

    Criação da entidade Order

--- src/main/java/com/example/projeto/entities/User.java
+++ src/main/java/com/example/projeto/entities/User.java
@@ -1,13 +1,18 @@
 package com.example.projeto.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
@@ -21,6 +26,10 @@ public class User implements Serializable{
 	private String fone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
+
 	public User() {
 		
 	}
@@ -71,6 +80,10 @@ public class User implements Serializable{
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
