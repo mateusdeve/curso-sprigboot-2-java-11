f153ea16b9fb9ac0f4fcb3ea908c85d2cb8e3930
Mateus Pires <mateus.dev.ti@gmail.com>
2020-07-21 17:01:45 -0300

    Upadate users

--- src/main/java/com/example/projeto/resources/UserResource.java
+++ src/main/java/com/example/projeto/resources/UserResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,5 +49,10 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user){
+		user = service.update(id, user);
+		return ResponseEntity.ok().body(user);
+	}
 
 }
